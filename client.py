553a46ffbfc7cf21bcac0833aabe40050b98d4d5
Zamp98 <33207987+Zamp98@users.noreply.github.com>
2021-03-26 19:02:24 -0300

    a

--- client.py
+++ client.py
@@ -1,15 +1,12 @@
 import socket
-import base64
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 8080        # The port used by the server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-
 while True:
 	x = input("Digite alguma coisa")
-	x = base64.b64encode(bytes(str(x), 'utf-8'))
-	s.sendall(str(x).encode('ascii')) # manda a mensagem
+	s.sendall(x.encode('utf-8')) # manda a mensagem
 	data = s.recv(1024) # le a resposta
-	print('Received', data)
+	print('Received', str(data))
--- server.py
+++ server.py
@@ -1,36 +1,34 @@
 import socket
-import base64
+
 
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 8080        # Port to listen on (non-privileged ports are > 1023)
-
-# create an INET, STREAMing socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def start():
+	# create an INET, STREAMing socket
+	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	# bind the socket to a public host, and a well-known port
-serversocket.bind((HOST, PORT))
+	serversocket.bind((HOST, PORT))
 	# become a server socket
-serversocket.listen(5)
-(serversocket, address) = serversocket.accept()
-print("conectou")
-while True:
-	# accept connections from outside
-	# now do something with the clientsocket
-	# in this case, we'll pretend this is a threaded server
-	data = serversocket.recv(1024)
+	serversocket.listen(5)
+	(serversocket, address) = serversocket.accept()
+	print("conectou")
+	while True:
+		# accept connections from outside
+		# now do something with the clientsocket
+		# in this case, we'll pretend this is a threaded server
+		data = serversocket.recv(1024)
+		if not data: # quando acabar a conexão
+			print('sem conexão')
+			serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			# bind the socket to a public host, and a well-known port
+			serversocket.bind((HOST, PORT))
+			# become a server socket
+			serversocket.listen(5)
+			(serversocket, address) = serversocket.accept()
+		serversocket.sendall(data) # mandar mensagem
 
-	print("received:", data)
-	if not data: # quando acabar a conexão
-		print('sem conexão')
-		serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		# bind the socket to a public host, and a well-known port
-		serversocket.bind((HOST, PORT))
-		# become a server socket
-		serversocket.listen(5)
-		(serversocket, address) = serversocket.accept()
-	serversocket.sendall(data) # mandar mensagem
-
-
+start()
 # funções separadas para erro
 
 # 
