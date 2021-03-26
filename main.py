64ee9a91a648e24e7fe09b8913bd4018a984cb2d
Zamp98 <33207987+Zamp98@users.noreply.github.com>
2021-03-26 17:59:51 -0300

    usando base64

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
+import errno
 import sys
 import runpy
 import user
 import input_output
-import server
 import os
 import time
 
 def checkUsers():
     r = input_output.loadUsers("users.txt")
+    print("Diretorio corrente:",os.getcwd())
     for i in r:
         i.print() #cria o arquivo com o nome i
         try:#tenta verificar se a caixa do usuário existe
-            f = open(i.getLogin()+'Inbox.txt', 'r')
+            f = open(os.path.join(os.getcwd()+'\data', i.getLogin()+ 'Inbox.txt'), 'r')
             print("caixa existente")
             f.close()
         except:
             print("caixa criada")
-            f = open(i.getLogin()+'Inbox.txt', 'w')
+            if not os.path.exists(os.getcwd()+'\data'):
+                try:
+                    os.makedirs(os.getcwd()+'\data')
+                except OSError as exc: # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        raise
+            f = open(os.path.join(os.getcwd()+'\data', i.getLogin()+ 'Inbox.txt'), 'w')
             f.close()
 
 
 def main():
     checkUsers()
     print(sys.platform)
-    runpy.run_module(mod_name='server')
-    print("Iniciando servidor...")
-    time.sleep(1)
-    print("Iniciando cliente...")
-    runpy.run_module(mod_name='client')
+    #runpy.run_module(mod_name='server')
+    #print("Iniciando servidor...")
+    #time.sleep(1)
+    #print("Iniciando cliente...")
+    #runpy.run_module(mod_name='client')
 main()
